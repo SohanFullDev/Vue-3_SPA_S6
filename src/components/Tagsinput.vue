1ff4aac1d7bc932223db53e5745c58230324571f
ProgrammerSohan <sohanofficial2022@gmail.com>
2024-01-10 15:24:05 +0600

    directives & attribute bindings_a_30

--- src/components/Tagsinput.vue
+++ src/components/Tagsinput.vue
@@ -1,3 +1,18 @@
 <template>
+    <!-- <div>{{ tags }}</div>
+    <div>{{ tags.length}}</div> -->
+    <div>{{ 2+2 }}</div>
+    <div>{{ tags[0].toUpperCase()}}</div>
+
     <input type="text" />
 </template>
+<script>
+
+export default {
+    data: () =>({
+        tags:["vue", "react", "angular"]
+    })
+
+}
+
+</script>
