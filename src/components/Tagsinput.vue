80e8b46c4ce5448cd0d74e9731bf1c3c973bd593
ProgrammerSohan <sohanofficial2022@gmail.com>
2024-01-11 12:50:14 +0600

    styling the tags input component*37

--- src/components/Tagsinput.vue
+++ src/components/Tagsinput.vue
@@ -1,85 +1,103 @@
 <template>
-  
-    <div v-for="(tag, index) in tags">
-        {{tag}}
-     <a @click.prevent="removeTag(index)" href="#">&times;</a>
+    <div class="tags-input-wrapper">
+      <span class="tag-item" v-for="(tag, index) in tags" :key="index">
+        {{ tag }}
+        <a class="remove-tag" @click.prevent="removeTag(index)" href="#"
+          >&times;</a
+        >
+      </span>
+      <input
+        class="tag-input"
+        type="text"
+        v-model.trim="newTag"
+        @keydown.enter="addNewTag"
+        @keydown.delete="removeLastTag"
+        @keydown.tab.prevent="addNewTag"
+        :class="{ 'tag-exists': isTagExists }"
+      />
     </div>
-    <hr>
-    {{ newTag }}
-
-  <!--   <input 
-    type="text" 
-    v-model.trim="newTag"
-    @keydown.enter="addNewTag"
-    @keydown.delete="removeLastTag"
-    @keydown.tab.prevent="addNewTag"
-    style="color:red"
-    
-    /> -->
-    <input 
-    type="text" 
-    v-model.trim="newTag"
-    @keydown.enter="addNewTag"
-    @keydown.delete="removeLastTag"
-    @keydown.tab.prevent="addNewTag"
-    :class="{ 'tag-exists': isTagExists }"
-    
-    />
-    
-</template>
-<script>
-
-export default {
-    data: () =>({
-        //tags:["vue", "react", "angular"]
-        //tags:[]
-        tags:["vue", "react", "angular"],
-        newTag: "preact"
+  </template>
+  
+  <script>
+  export default {
+    data: () => ({
+      tags: ["vue", "react", "angular"],
+      newTag: "",
     }),
     watch: {
-        newTag(newVal){
-            //console.log(newVal, oldVal);
-            if(newVal.indexOf(",") > -1){
-                this.newTag = this.newTag.slice(0, -1)//ei line chara aghe ekbar test hobe
-                this.addNewTag();
-            }
+      newTag(newVal) {
+        if (newVal.indexOf(",") > -1) {
+          this.newTag = this.newTag.slice(0, -1);
+          this.addNewTag();
         }
-
+      },
     },
-    computed:{
-        isTagExists(){
-            return this.tags.includes(this.newTag);
-        }
-
+    computed: {
+      isTagExists() {
+        return this.tags.includes(this.newTag);
+      },
     },
     methods: {
-
-        addNewTag(){
-            //if(this.newTag && !this.tags.includes(this.newTag)){
-                if(this.newTag && !this.isTagExists){
-                this.tags.push(this.newTag)
-                this.newTag = ""
-
-            }
-            
-        },
-        removeTag(index){
-            this.tags.splice(index, 1)
-        },
-        removeLastTag(){
-            if(this.newTag.length==0){
-                this.removeTag(this.tags.length - 1)
-            }
+      addNewTag() {
+        if (this.newTag && !this.isTagExists) {
+          this.tags.push(this.newTag);
+          this.newTag = "";
         }
-    }
-
-}
-
-</script>
-<style scoped>
-.tag-exists{
+      },
+      removeTag(index) {
+        this.tags.splice(index, 1);
+      },
+      removeLastTag() {
+        if (this.newTag.length === 0) {
+          this.removeTag(this.tags.length - 1);
+        }
+      },
+    },
+  };
+  </script>
+  
+  <style scoped>
+  .tag-input.tag-exists {
     color: red;
     text-decoration: line-through;
-}
-
-</style>
+  }
+  .tags-input-wrapper {
+    background: #fff;
+    padding: 0.5em;
+    border: 1px solid #dbdbdb;
+    display: flex;
+    flex-wrap: wrap;
+    align-items: center;
+    min-height: 36px;
+    box-sizing: border-box;
+  }
+  
+  .tag-item {
+    color: #212529;
+    background-color: #eee;
+    margin-right: 0.3em;
+    padding: 0.25em 0.4em;
+    font-size: 75%;
+    line-height: 1;
+    text-align: center;
+    white-space: nowrap;
+    overflow: hidden;
+    text-overflow: ellipsis;
+    padding-right: 1.25em;
+    padding-left: 0.6em;
+  }
+  
+  .tag-input {
+    color: #495057;
+    flex: 1;
+    background: transparent;
+    border: none;
+  }
+  
+  .tag-input:focus {
+    outline: none;
+  }
+  a.remove-tag {
+    text-decoration: none;
+  }
+  </style>
